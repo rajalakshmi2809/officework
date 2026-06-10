7c06116506288886fa37563a91302ba187d3ea2e
rajalakshmi2809 <rajalakshmi280903@gmail.com>
2026-06-10 09:29:19 +0530

    updated

---
+++ React/registrationform/src/App.jsx
@@ -0,0 +1,22 @@
+﻿import ProductSearch from "./component/ProductSearch"
+import RegistrationForm from "./component/RegistrationForm"
+
+const App = () => {
+  return (
+    <>
+    <div>
+      <div>
+        <h1>Task 1</h1>
+        <RegistrationForm />
+        </div>
+        <div>
+        <h1>Task 2</h1>
+        <ProductSearch/>
+        </div>
+      
+      </div>
+    </>
+  )
+}
+
+export default App
